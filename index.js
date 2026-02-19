991d9d54acf0dc310abbad5850e9dc1721391903
Amina <minassouane@gmail.com>
2026-02-19 22:20:02 +0100

    Handle 404 error

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.get("/about", (req, res) =>
 app.get("/contact-me", (req, res) =>
   res.sendFile(path.join(__dirname, "contact-me.html")),
 );
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
